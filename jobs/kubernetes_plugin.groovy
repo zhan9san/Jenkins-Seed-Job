43e70de103df1dca9e0dfff400810851fbb58bb3
Jack Zhang <jack4zhang@gmail.com>
2021-07-01 18:41:47 +0800

    add kaniko

--- jobs/kubernetes_plugin.groovy
+++ jobs/kubernetes_plugin.groovy
@@ -1,28 +1,32 @@
-multibranchPipelineJob('kubernetes-plugin') {
-  branchSources {
-    branchSource {
-      source {
-        git {
-          // https://issues.jenkins.io/browse/JENKINS-48571
-          id('Jenkins-Sample-Job') // IMPORTANT: use a constant and unique identifier
-          remote('https://github.com/zhan9san/Jenkins-Sample-Job')
-          traits {
-            gitBranchDiscovery()
+def projects = ['kubernetes-plugin', 'kaniko']
+
+for(project in projects) {
+  multibranchPipelineJob(project) {
+    branchSources {
+      branchSource {
+        source {
+          git {
+            // https://issues.jenkins.io/browse/JENKINS-48571
+            id('Jenkins-Sample-Job') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/zhan9san/Jenkins-Sample-Job')
+            traits {
+              gitBranchDiscovery()
+            }
           }
         }
-      }
-      strategy {
-        allBranchesSame {
-          props {
-            suppressAutomaticTriggering()
+        strategy {
+          allBranchesSame {
+            props {
+              suppressAutomaticTriggering()
+            }
           }
         }
       }
     }
-  }
-  factory {
-    workflowBranchProjectFactory {
-      scriptPath('kubernetes-plugin/Jenkinsfile')
+    factory {
+      workflowBranchProjectFactory {
+        scriptPath("$project/Jenkinsfile")
+      }
     }
   }
 }
