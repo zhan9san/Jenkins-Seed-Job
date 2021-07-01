a1ad997333be845bf4b56e1c9bcba97424297429
Jack Zhang <jack4zhang@gmail.com>
2021-07-01 18:17:00 +0800

    Add one job

---
+++ jobs/kubernetes-plugin.groovy
@@ -0,0 +1,28 @@
+multibranchPipelineJob('test-pipeline') {
+  branchSources {
+    branchSource {
+      source {
+        git {
+          // https://issues.jenkins.io/browse/JENKINS-48571
+          id('Jenkins-Sample-Job') // IMPORTANT: use a constant and unique identifier
+          remote('https://github.com/zhan9san/Jenkins-Sample-Job')
+          traits {
+            gitBranchDiscovery()
+          }
+        }
+      }
+      strategy {
+        allBranchesSame {
+          props {
+            suppressAutomaticTriggering()
+          }
+        }
+      }
+    }
+  }
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('kubernetes-plugin/Jenkinsfile')
+    }
+  }
+}
